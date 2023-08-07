bcb7f2d151437a46e341e7b0139ae0c62930d24e
Ben <me@bpmct.net>
2023-08-07 23:21:21 +0000

    use full version

--- .github/workflows/aws/submit_ecr.sh
+++ .github/workflows/aws/submit_ecr.sh
@@ -8,9 +8,15 @@ fi
 
 SAFE_VERSION="${VERSION/v/""}"
 
+if [[ -n $APPEND_VERSION ]]; then
+    FULL_VERSION="${SAFE_VERSION}${APPEND_VERSION}"
+else
+    FULL_VERSION="${SAFE_VERSION}"
+fi
+
 # Publish image
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 709825985650.dkr.ecr.us-east-1.amazonaws.com
-export ecr_location=$ECR_IMAGE_REPO:v$SAFE_VERSION
+export ecr_location=$ECR_IMAGE_REPO:v$FULL_VERSION
 docker pull ghcr.io/coder/coder:v$SAFE_VERSION
 docker tag ghcr.io/coder/coder:v$SAFE_VERSION $ecr_location
 docker push $ecr_location
@@ -26,8 +32,8 @@ tar -xvf coder_helm_$SAFE_VERSION.tgz
 # Replace coder.image.repo with $ECR_IMAGE_REPO
 sed -i 's|repo: "ghcr.io/coder/coder"|repo: "'"$ECR_IMAGE_REPO"'"|' "./coder/values.yaml"
 # Replace coder.image.tag with v$SAFE_VERSION
-sed -i 's|tag: ""|tag: "v'"$SAFE_VERSION"'"|' "./coder/values.yaml"
+sed -i 's|tag: ""|tag: "v'"$FULL_VERSION"'"|' "./coder/values.yaml"
 aws ecr get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin 709825985650.dkr.ecr.us-east-1.amazonaws.com
-helm chart save ./coder 709825985650.dkr.ecr.us-east-1.amazonaws.com/coder/coderv2-marketplace:v$SAFE_VERSION
-helm chart push 709825985650.dkr.ecr.us-east-1.amazonaws.com/coder/coderv2-marketplace:v$SAFE_VERSION
+helm chart save ./coder 709825985650.dkr.ecr.us-east-1.amazonaws.com/coder/coderv2-marketplace:v$FULL_VERSION
+helm chart push 709825985650.dkr.ecr.us-east-1.amazonaws.com/coder/coderv2-marketplace:v$FULL_VERSION
 aws ecr describe-images --registry-id 709825985650 --repository-name coder/coderv2-marketplace --region us-east-1
