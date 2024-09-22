d9f1189811c2913dab8e4e6f53defddd0e87b378
Jame <Jamelagalinato8@gmail.com>
2024-09-22 21:23:02 +0800

    Create activity1.js

---
+++ activity1.js
@@ -0,0 +1,9 @@
+let Name = "Jamela Galinato";
+let WhatDoYouWannaBecomeInYourLife = programmer;
+let gender = female;
+let FacebookAcct = "Jamela Baturi Galinato"
+
+console.log(Name);
+console.log(WhatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(FacebookAcct);
